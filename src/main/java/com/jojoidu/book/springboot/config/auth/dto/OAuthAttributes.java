62c02fb2361b1c50be35ef7c6f9380018b5bc3c5
song <thdtn816@gmail.com>
2023-04-18 22:36:55 +0900

    네이버 소셜 로그인 구현 완료

--- src/main/java/com/jojoidu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoidu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -9,14 +9,14 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
-    private final Map<String, Object> attributes;
-    private final String nameAttributekey;
-    private final String name;
-    private final String email;
-    private final String picture;
+    private Map<String, Object> attributes;
+    private String nameAttributekey;
+    private String name;
+    private String email;
+    private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,String nameAttributekey, String name, String email, String picture){
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributekey, String name, String email, String picture){
         this.attributes = attributes;
         this.nameAttributekey = nameAttributekey;
         this.name = name;
@@ -25,10 +25,27 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String,Object> attributes) {
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String,Object> attributes){
+
+        Map<String,Object> response = (Map<String,Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributekey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String,Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
